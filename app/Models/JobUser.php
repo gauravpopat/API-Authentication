f8e36afcb17eb9e990b728bbe8cdcc4a9bf3d5d7
Gaurav <gauravp@zignuts.com>
2023-03-22 18:35:08 +0530

    added field in fillable property

--- app/Models/JobUser.php
+++ app/Models/JobUser.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class JobUser extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id','job_id','is_confirm','resume'];
+
+    public function jobs()
+    {
+        return $this->belongsTo(Job::class,'job_id');
+    }
 }
