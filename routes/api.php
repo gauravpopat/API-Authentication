9c86653d7c371a8485b60cf591eb83ad2539a12d
Gaurav <gauravp@zignuts.com>
2023-03-22 18:34:36 +0530

    Create routes for company crud, job crud

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\JobController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserJobController;
+use App\Models\JobUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,16 +26,16 @@ use Illuminate\Support\Facades\Route;
 
 
 //Custom Message When User is Unauthenticated -> app\http\middleware\authenticate.php
-Route::get('unauthenticated',function(){
-    return error('unauthenticated','','unauthenticated');
+Route::get('unauthenticated', function () {
+    return error('unauthenticated', '', 'unauthenticated');
 })->name('unauthenticated');
 
 //Guest User
 Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
-    Route::get('verify-email/{verificationCode}','verifyEmail');
-    Route::post('forgot-password-link','forgotPasswordLink');
-    Route::post('forgot-password','forgotPassword');
+    Route::get('verify-email/{verificationCode}', 'verifyEmail');
+    Route::post('forgot-password-link', 'forgotPasswordLink');
+    Route::post('forgot-password', 'forgotPassword');
     Route::post('login', 'login');
 });
 
@@ -43,4 +47,32 @@ Route::middleware('auth:api')->group(function () {
         Route::post('logout', 'logout');
         Route::get('delete-user', 'delete');
     });
+
+    //JobUsers : Apply for job
+    Route::controller(UserJobController::class)->prefix('apply')->group(function () {
+        Route::get('job-list', 'list');
+        Route::post('job-apply', 'create');
+    });
+});
+
+//Company
+Route::controller(CompanyController::class)->prefix('company')->group(function () {
+    Route::get('list', 'list');
+    Route::post('create', 'create');
+    Route::post('update', 'update');
+    Route::get('show/{id}', 'show');
+    Route::post('delete/{id}', 'delete');
+});
+
+//Jobs
+Route::controller(JobController::class)->prefix('job')->group(function () {
+    Route::get('list', 'list');
+    Route::post('create', 'create');
+    Route::post('update', 'update');
+    Route::get('show/{id}', 'show');
+    Route::post('delete/{id}', 'delete');
+
+    //Accept the user application for job.
+    Route::get('list-application', 'listApplication');
+    Route::post('approve-application', 'approveApplication');
 });
